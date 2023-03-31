7fcf479a80eeab694f87f868caac362de2826e14
caiohasouza <caio@ganex.com.br>
2023-03-31 11:58:15 -0400

    fix: adjusting enable_default_standards default value

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "enable_default_standards" {
   description = "Whether to enable the security standards that Security Hub has designated as automatically enabled including: `AWS Foundational Security Best Practices v1.0.0` and `CIS AWS Foundations Benchmark v1.2.0`"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "enable_notifications" {
